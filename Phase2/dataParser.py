4917a03d45f58aab35f016301f1f80d10ab1a344
zen1405 <shreyaskanjalkar@gmail.com>
2022-11-01 00:21:56 -0400

    Data loader and Network for Phase 2 done

---
+++ Phase2/dataParser.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+import json
+import numpy as np
+
+class Data():
+    def __init__(self, path) -> None:
+        self.path = path
+
+
+    def readImages(self, type):
+        images = []
+        image_order = sorted(os.listdir(self.path+f'{type}/'))
+        for image in image_order:
+            img = cv2.imread(os.path.join(self.path+f'{type}/', image))
+            images.append(img)
+        return images
+
+    def readJson(self, type):
+        f = open(self.path+type)
+
+        data = json.load(f)
+        transformationMatrix = []
+        rotation = []
+        for frame in data['frames']:
+            rotation.append(float(frame["rotation"]))
+            transform_matrix = frame["transform_matrix"]
+            numpyTransform = np.asarray(transform_matrix, dtype=np.float32)
+            transformationMatrix.append(numpyTransform.T)
+
+        f.close()
+        return rotation, transformationMatrix
+
