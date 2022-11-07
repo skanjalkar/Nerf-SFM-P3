dfa905f0746bfa83d8ade2ad1dc6c133c22b88dc
zen1405 <shreyaskanjalkar@gmail.com>
2022-11-07 15:15:11 -0500

    nerf testing

--- Phase2/Wrapper.py
+++ Phase2/Wrapper.py
@@ -1,8 +1,13 @@
 import torch
+import numpy as np
 import torch.nn as nn
 from dataParser import *
+import matplotlib.pyplot as plt
+import pry
+from train import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = "cpu"
 
 
 def main():
@@ -10,7 +15,27 @@ def main():
     data = Data("lego-20221031T225340Z-001/lego/")
     trainImages = data.readImages('train')
     trainRotation, trainTransformation = data.readJson('transforms_train.json')
-    
+
+    focalLength = np.array([138.8888789])
+    focalLength = torch.from_numpy(focalLength).to(device)
+
+    height, width = trainImages.shape[1:3]
+    near_threshold = 2.
+    far_threshold = 6.
+
+    print(trainImages.shape)
+    # pry()
+
+    trainTransformation = torch.from_numpy(trainTransformation).to(device)
+
+    trainImages = torch.from_numpy(trainImages[:, ..., :3]).to(device)
+
+    trainObj = Train(height, width, trainImages, trainTransformation, focalLength, near_threshold, far_threshold)
+    trainObj.train(6, 32, 16384)
+    # ro, rd = trainObj.get_ray_bundle(trainTransformation[0])
+    # query_points, depth_values = trainObj.computeQueryPoints(ro, rd, 32)
+
+
     # testImages = data.readImages('test')
     # testRotation, testTransformation = data.readJson('transforms_train.json')
 
---
+++ Phase2/train.py
@@ -0,0 +1,163 @@
+import numpy as np
+import torch
+from Network import *
+import pry
+import matplotlib.pyplot as plt
+
+
+class Train():
+    def __init__(self, height, width, images, poses, focal_length, near_threshold, far_threshold) -> None:
+        self.height = height
+        self.width = width
+        self.images = images
+        self.poses = poses
+        self.focalLength = focal_length
+        self.near_threshold = near_threshold
+        self.far_threshold = far_threshold
+
+    def meshgridxy(self, tensor1, tensor2):
+        ii, jj = torch.meshgrid(tensor1, tensor2)
+        return ii.transpose(-1, -2), jj.transpose(-1, -2)
+
+    def get_ray_bundle(self, pose):
+        ii, jj = self.meshgridxy(torch.arange(self.width).to(pose), torch.arange(self.height).to(pose))
+        directions = torch.stack([(ii - self.width*0.5)/self.focalLength, -(jj-self.height*0.5)/self.focalLength, -torch.ones_like(ii)], dim=-1)
+        ray_directions = torch.sum(directions[..., None, :]*pose[:3, :3], dim=-1)
+        ray_origins = pose[:3, -1].expand(ray_directions.shape)
+        # pry()
+        return ray_origins, ray_directions
+
+    def computeQueryPoints(self, ray_origins, ray_directions, depth_samples_per_ray):
+        depth_values = torch.linspace(self.near_threshold, self.far_threshold, depth_samples_per_ray).to(ray_origins)
+        noise_shape = list(ray_origins.shape[:-1])+[depth_samples_per_ray]
+        depth_values = depth_values + torch.rand(noise_shape).to(ray_origins)*(self.far_threshold-self.near_threshold)/depth_samples_per_ray
+        query_points = ray_origins[..., None, :] + ray_directions[..., None, :]*depth_values[..., :, None]
+        # pry()
+        return query_points, depth_values
+
+    def positionalEncoding(self, tensor, num_encoding_functions=6):
+        encoding = [tensor]
+        frequencyBands = 2.00 ** torch.linspace(
+            0.0,
+            num_encoding_functions - 1,
+            num_encoding_functions,
+            dtype=tensor.dtype,
+            device=tensor.device,
+        )
+
+        for frequency in frequencyBands:
+            for function in [torch.sin, torch.cos]:
+                encoding.append(function(tensor*frequency))
+
+        return torch.cat(encoding, dim=-1)
+
+    def miniBatches(self, ray_bundle, chunksize):
+        return [ray_bundle[i:i+chunksize] for i in range(0, ray_bundle.shape[0], chunksize)]
+
+    def train(self, num_encoding_functions, depth_samples_per_ray, chunksize):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        lr = 5e-3
+        num_iterations = 1000
+        encode = lambda x: self.positionalEncoding(x, num_encoding_functions=num_encoding_functions)
+        model = NerfModel(encoding=num_encoding_functions)
+        model = model.to(device)
+
+        # nameless function
+        # same as def encode(x) -> self.postionalEncoding(x, num_econding_functions)
+        # lambda x : x**2 + num_iterations (access to environment or local variables instead of passing them as arguments)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        chunksize = 16384
+
+        #seed
+        seed = 45
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+        iters = []
+        psnrs = []
+
+        for i in range(num_iterations):
+            randomImageIndex = np.random.randint(self.images.shape[0])
+            randomImage = self.images[randomImageIndex].to(device)
+            pose = self.poses[randomImageIndex].to(device)
+
+            # runNerfModel
+            rgbPredicted, depthMap, accuracyMap = self.runNerfModel(depth_samples_per_ray, encode, pose, chunksize, model)
+            loss = torch.nn.functional.mse_loss(rgbPredicted, randomImage)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if i%150 == 0:
+                test_image = self.images[-1].to(device)
+                test_pose = self.poses[-1].to(device)
+
+                rgbPredicted, _, _ = self.runNerfModel(depth_samples_per_ray, encode, test_pose, chunksize, model)
+                loss = torch.nn.functional.mse_loss(rgbPredicted, test_image)
+                print("loss:", loss.item())
+                psnr = -10.*torch.log10(loss)
+
+                iters.append(i)
+                psnrs.append(psnr)
+
+                plt.figure(figsize=(10, 4))
+                plt.subplot(121)
+                plt.imshow(rgbPredicted.detach().cpu().numpy())
+                plt.title(f'iteartions{i}')
+                plt.subplot(122)
+                plt.plot(iters, psnrs)
+                plt.title("PSNR")
+                plt.show()
+        print('Done!')
+
+
+    def runNerfModel(self, depth_samples_per_ray, encode, pose, chunksize, model):
+        ray_origins, ray_directions = self.get_ray_bundle(pose)
+
+        query_points, depth_values = self.computeQueryPoints(ray_origins, ray_directions, depth_samples_per_ray)
+
+        flattened_query_points = query_points.reshape((-1, 3))
+
+        encded_points = encode(flattened_query_points)
+
+        batches = self.miniBatches(encded_points, chunksize)
+        predictions = []
+
+        for batch in batches:
+            predictions.append(model(batch))
+        radianceField_flattened = torch.cat(predictions, dim=0)
+
+        unflattened_shape = list(query_points.shape[:-1]) + [4]
+        radianceField = torch.reshape(radianceField_flattened, unflattened_shape) # c(r)
+
+        rgbPredicted,depthPredicted, accuracy = self.renderVolumeDensity(radianceField, ray_origins, ray_directions, depth_values)
+
+        return rgbPredicted, depthPredicted, accuracy
+
+    def cumProducts(self, tensor):
+        cumProd = torch.cumprod(tensor, -1)
+        cumProd = torch.roll(cumProd, 1, -1)
+        cumProd[..., 0] = 1
+        return cumProd
+
+    def renderVolumeDensity(self, radianceField, ray_origins, depthValues):
+        sigma = torch.nn.functional.relu(radianceField[..., 3])
+        rgb = torch.sigmoid(radianceField[..., :3])
+        oneE10 = torch.tensor([1e10], dtype=ray_origins.dtype, device=ray_origins.device)
+        dists = torch.cat((depthValues[..., 1:]-depthValues[..., :-1]), oneE10.expand(depthValues[..., :1].shape), dim=-1)
+        alpha = 1. - torch.exp(-sigma*dists)
+        weights = alpha * self.cumProducts(1. - alpha+1e10)
+        rgb_map = (weights[..., None]*rgb).sum(dim=-2)
+        depthMap = (weights*depthValues).sum(dim=-1)
+        acc_map = weights.sum(-1)
+
+        return rgb_map, depthMap, acc_map
+
+
+
+
+
+
+
+
+
